5c96f99278728de3cd00fe3d7bb3765d63e3a344
hhicks13 <hhicks13@vols.utk.edu>
2017-12-18 19:04:17 -0500

    "test commit"

--- ethereumj-core/src/main/java/org/nominis/AccountGenerator.java
+++ ethereumj-core/src/main/java/org/nominis/AccountGenerator.java
@@ -30,7 +30,7 @@ public class AccountGenerator {
      * ArrayList would be for containing the info
      * tuple would be for each pair of account
      *
-     *k;lk;lk
+     *k;lk;lkdds
      * ------------
      * The getGeneratedAccounts is able to provide a list of accounts.
      * However, you would not be able to get balance.
